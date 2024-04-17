ce6d59f28b517e3b4be51a5c0d13b9d6e400331c
Adit Chopra <98331414+aditchopra18@users.noreply.github.com>
2024-04-16 21:55:12 -0400

    Second Commit

--- server.py
+++ server.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def LandingPage():
     return render_template("index.html");
 
+@app.route("/register")
+def RegisterPage():
+    return render_template("register.html");
+
 if __name__ == "__main__":
     app.run(debug = True)
